38dd82974e559a0628a4b71893fbd0384817ab61
James Thorne <james@jthorne.co.uk>
2019-03-22 14:43:26 +0000

    made predictions script more verbose

--- predict.sh
+++ predict.sh
@@ -5,6 +5,8 @@ root_dir=/local/fever-common
 
 ln -s $root_dir/data data
 
+echo "start evidence retrieval"
+
 python -m fever.evidence.retrieve \
     --index $root_dir/data/index/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz \
     --database $root_dir/data/fever/fever.db \
@@ -13,6 +15,7 @@ python -m fever.evidence.retrieve \
     --max-page 5 \
     --max-sent 5
 
+echo "start prediction"
 python -m allennlp.run predict \
     https://jamesthorne.co.uk/fever/fever-da.tar.gz \
     /tmp/ir.$(basename 1) \
@@ -22,6 +25,7 @@ python -m allennlp.run predict \
     --cuda-device ${CUDA_DEVICE:-$default_cuda_device} \
     --silent
 
+echo "prepare submission"
 python -m fever.submission.prepare \
     --predicted_labels /tmp/labels.$(basename $1) \
     --predicted_evidence /tmp/ir.$(basename 1) \
